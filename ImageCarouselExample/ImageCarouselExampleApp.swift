fbd21cda058e5ee07e064c7f64994545af5631d5
katafuchix <katafuchix@gmail.com>
2023-03-18 21:31:44 +0900

    Initial Commit

---
+++ ImageCarouselExample/ImageCarouselExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCarouselExampleApp.swift
+//  ImageCarouselExample
+//
+//  Created by cano on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct ImageCarouselExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
